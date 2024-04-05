e3bf52d5aa950e00d5df0c2ec821fa30cb320ce0
ThorTheTroll <hasannawadd@gmail.com>
2024-04-05 15:18:48 +0300

    commitmessage first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='navbar'>
+        <div class="logo">
+          <img src="./BitsLogo.png" alt="logo" />
+        </div>
+        <ul className='navbar-btns'>
+          <li>
+            Home
+          </li>
+          <li>
+            Products
+          </li>
+          <li>
+            Careers
+          </li>
+          <li>
+            Contact
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
